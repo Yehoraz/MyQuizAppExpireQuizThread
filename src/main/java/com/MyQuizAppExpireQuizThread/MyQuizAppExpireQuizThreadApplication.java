ba30c1a89e940719592ddd208c52487bf4764872
Yehoraz <46538668+Yehoraz@users.noreply.github.com>
2019-11-14 19:52:20 +0200

    commit #2, change thread to scheduled

--- src/main/java/com/MyQuizAppExpireQuizThread/MyQuizAppExpireQuizThreadApplication.java
+++ src/main/java/com/MyQuizAppExpireQuizThread/MyQuizAppExpireQuizThreadApplication.java
@@ -2,36 +2,16 @@ package com.MyQuizAppExpireQuizThread;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.MyQuizAppExpireQuizThread.thread.QuizExpirationThread;
-
+@EnableScheduling
 @SpringBootApplication
 @RestController
 public class MyQuizAppExpireQuizThreadApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyQuizAppExpireQuizThreadApplication.class, args);
-		
-		startDeleteThread();
-	}
-	
-	public static void startDeleteThread() {
-		Thread t1 = new Thread(new QuizExpirationThread());
-		t1.start();
-	}
-	
-	@PutMapping("/restartDeleteThread")
-	public void restartDeleteThread(){
-		if(QuizExpirationThread.getIfQuitFlag() == true) {
-			startDeleteThread();
 		}
-	}
-	
-	@PutMapping("endDeleteThread")
-	public void endDeleteThread(){
-		QuizExpirationThread.endDeleteThread();
-	}
 
 }
---
+++ src/main/java/com/MyQuizAppExpireQuizThread/thread/QuizExpirationScheduled.java
@@ -0,0 +1,32 @@
+package com.MyQuizAppExpireQuizThread.thread;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@PropertySource(value="classpath:info.properties")
+public class QuizExpirationScheduled {
+	
+	@Autowired
+	private Environment env;
+
+	private RestTemplate restTemplate = new RestTemplate();
+	private String url;
+	
+	@PostConstruct
+	private void s() {
+		url = env.getProperty("url.mainAppExpiredRequestURL");
+	}
+	
+	@Scheduled(fixedRate = (1000*60*60*24))
+	private void task() {
+		restTemplate.delete(url);
+	}
+	
+}
