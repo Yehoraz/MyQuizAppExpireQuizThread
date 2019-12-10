e4d0a7e818209758d43aaebba52b5a7efa7385c6
Yehoraz <46538668+Yehoraz@users.noreply.github.com>
2019-12-10 15:56:32 +0200

    commit #5, small fix

--- src/main/java/com/MyQuizAppExpireQuizThread/MyQuizAppExpireQuizThreadApplication.java
+++ src/main/java/com/MyQuizAppExpireQuizThread/MyQuizAppExpireQuizThreadApplication.java
@@ -3,11 +3,9 @@ package com.MyQuizAppExpireQuizThread;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableScheduling
 @SpringBootApplication
-@RestController
 public class MyQuizAppExpireQuizThreadApplication {
 
 	public static void main(String[] args) {
